072f7751ea8a1e8a1c55b4af05693217789c8bd0
Ibrahim Alsagaby <br5osh@gmail.com>
2026-08-08 02:52:53 +0300

    fix: propagate cancellation from process signals

--- cmd/gew/main.go
+++ cmd/gew/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
 
 	"gew/internal/cli"
 )
 
 func main() {
-	if err := cli.Run(os.Args[1:], os.Stdout, os.Stderr); err != nil {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+	if err := cli.RunContext(ctx, os.Args[1:], os.Stdout, os.Stderr); err != nil {
 		fmt.Fprintf(os.Stderr, "gew: %v\n", err)
 		os.Exit(1)
 	}
